0995d84ee868a543a458e7c323e019c4226c8026
Teodor Ivanov <teodor.ivanov1608@gmail.com>
2023-07-04 22:28:08 +0300

    meshGradientBG

---
+++ WinWidgets2/MeshBG/meshGradientBGScripts.js
@@ -0,0 +1,8 @@
+import { Gradient } from './Gradient.js'
+
+const gradient = new Gradient()
+
+console.log(gradient)
+
+// Call `initGradient` with the selector to your canvas
+gradient.initGradient('#gradient-canvas')
